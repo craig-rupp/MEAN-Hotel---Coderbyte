6e8af4a18d2b0475b49039f87eab58d47ce79579
Craig Rupp <craigrupp@utexas.edu>
2017-01-19 13:56:10 -0600

    add register template and controller, add both to app angular for continuity

--- public/angular-app/app-angular.js
+++ public/angular-app/app-angular.js
@@ -12,6 +12,11 @@ function config($routeProvider){
 			templateUrl :'angular-app/hotel-display/hotel.html',
 			controller: HotelController,
 			controllerAs: 'vm'
+		})
+		.when('/register', {
+			templateUrl : 'angular-app/register/register.html',
+			controller : RegisterController, 
+			controllerAs : 'vm'
 		});
 }
  
