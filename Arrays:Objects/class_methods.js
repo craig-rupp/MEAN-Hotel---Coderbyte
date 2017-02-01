5ada2430c9f22b1b83c981c7a2736fcb7d8a1405
Craig Rupp <craigrupp@utexas.edu>
2017-02-01 12:48:02 -0600

    Adding JS class methods ES6

--- Arrays:Objects/class_methods.js
+++ Arrays:Objects/class_methods.js
@@ -23,3 +23,46 @@ var IPhone = new iPhone(5092802881);
 console.log(IPhone);
 IPhone.call();
 IPhone.email('craigrupp@utexas.edu');
+
+var a = {
+	n : 3,
+	b : 15, 
+	c : 24
+};
+for(var key in a){
+	console.log(a[key]);
+}
+console.log(a.n, a.b, a.c, a['n'], a['b'], a['c']);
+
+var d = {};
+for (var key in a) {
+	d[key] = a[key];
+}
+console.log(a, d);
+d.c = 8;
+console.log(a, d);
+
+//You are given a function that takes an object as an argument. Make a copy of the object and return the copy.
+//FOR IN LOOP allows looping through OBJECT 
+function copyTheObject(obj){
+    var newObj = {};
+    for(var key in obj){
+        newObj[key] = obj[key];
+    }
+    return newObj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
