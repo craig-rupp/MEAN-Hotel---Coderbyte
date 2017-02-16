61fefd0b89ac44b7eb9b05d4f4705e5b74766a2e
Craig Rupp <craigrupp@utexas.edu>
2017-02-16 17:17:05 -0600

    a few more coderbyte test solutions

--- coderbyte/js/test.js
+++ coderbyte/js/test.js
@@ -55,3 +55,62 @@ function FirstFactorial(num) {
    
 // keep this function call here 
 //FirstFactorial(readline());
+
+//Return next letter in alphabet from string, capitalize vowels
+var test_string = "hello";
+function nextNCapitalize(str) {
+	var string = str.split('');
+	for(var i = 0; i < string.length; i++){
+		switch(string[i]) {
+			case ' ':
+			break;
+			case 'z':
+			string[i] = 'a';
+			break;
+			case 'Z':
+			string[i] = 'A';
+			break;
+			default:
+			string[i] = String.fromCharCode(1 + string[i].charCodeAt(0));
+		}
+		switch(string[i]){
+			case 'a' : case 'e' : case 'i' : case 'o' : case 'u' :
+			string[i] = string[i].toUpperCase();
+		}
+	}
+	console.log(string.join(''));
+}
+
+
+nextNCapitalize(test_string);
+
+function letterChanges(str) {
+	var res = '';
+	for (var i = 0; i < str.length; i++){
+		var cur = str[i];
+		if(cur == 'z'){
+			cur = 'a';
+		} else if(str.charCodeAt(i) >= 'a'.charCodeAt(0) && str.charCodeAt(i) <= 'z'.charCodeAt(0)) {
+			cur = String.fromCharCode(str.charCodeAt(i) + 1);
+		}
+		if(cur == 'a' || cur == 'e' || cur == 'i' || cur == 'o' || cur == 'i'){
+			cur = cur.toUpperCase();
+		}
+		res += cur;
+	}
+	console.log(res);
+}
+letterChanges('daughter');
+
+//takes in numbers adds all integers in (4 = 1 + 2 + 3 + 4 = 10);
+function simpleAdding(number) {
+	var result = 0;
+	for(var i = 1; i <= number; i++){
+		result += i;
+	}
+	return result;
+}
+
+simpleAdding(4);
+
+
