77f66d0d8a02452f7a814bbbbe1b33d1595ddf1f
Craig Rupp <craigrupp@utexas.edu>
2017-02-17 14:06:58 -0600

    more coderbyte

--- coderbyte/js/test.js
+++ coderbyte/js/test.js
@@ -165,6 +165,18 @@ function moreSimpleSymbols(symb) {
 	return true;
 }
 
+//Friday 2/17 first coderbyte challenge
+function CheckNums(num1,num2) { 
+    if(num2 > num1){
+        return true;
+    } else if (num1 > num2) {
+        return false;
+    } else if (num1 === num2){
+        return -1;
+    }
+}
+
+
 
 
 
