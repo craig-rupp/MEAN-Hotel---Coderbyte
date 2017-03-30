b5c0e625795a0597b8aa7b2513b5beb32a2612ec
Craig Rupp <craigrupp@utexas.edu>
2017-03-30 11:55:51 -0500

    Initialy project commit for recipe-book

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component'
+
+@Component({
+  selector: 'rb-book-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  
+}
