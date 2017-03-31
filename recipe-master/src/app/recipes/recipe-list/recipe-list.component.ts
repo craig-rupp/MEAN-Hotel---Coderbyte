c68b71757d06f759789287392f1ed558d2c3a178
Craig Rupp <craigrupp@utexas.edu>
2017-03-30 19:38:49 -0500

    initial component add, first instance of Recipecomponents class

---
+++ recipe-master/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
+import { RecipeItemComponent } from './recipe-item.component';
+
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [];
+  recipe = new Recipe('Gonzaga', 'Final Four Baby', 'http://hollandbarstool.com/media/catalog/category/Gonzaga.jpg');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
