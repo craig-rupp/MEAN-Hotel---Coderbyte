58c718762bf97c6890eecc0529c4da67cd0a22a1
Craig Rupp <craigrupp@utexas.edu>
2017-04-12 18:42:05 -0500

    NG template work

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
+//import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'dir-root',
@@ -7,4 +8,10 @@ import { HighlightDirective } from './highlight.directive';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	private switch = true;
+	private items = [1, 2, 3, 4, 5];
+	private value = 100;
+	onSwitch(){
+		this.switch = !this.switch;
+	}
 }
