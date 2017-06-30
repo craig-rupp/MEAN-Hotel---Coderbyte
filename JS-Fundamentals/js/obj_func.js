4b497fda0bafbc24e0fb6a52729c839afff40079
Craig Rupp <craigrupp@utexas.edu>
2017-06-29 22:24:59 -0500

    Namespaces

--- JS-Fundamentals/js/obj_func.js
+++ JS-Fundamentals/js/obj_func.js
@@ -16,7 +16,56 @@ person.address = {
 		Basketball : 'Gonzaga Bulldogs',
 		Soccer : 'Seattle Sounders',
 		football : function(){
-			console.log('The Seahawks are the squad');
+			return ('The Seahawks were the squad');
 		}
 	}
 };
+
+function nWKid(object){
+	console.log('The ' + object.address.sports_teams['Soccer'] + ' are the recent MLS Champs');
+}
+
+function nwHawks(object){
+	console.log('In 2014, ' + object.address.sports_teams.football() + ' to be reckoned with');
+}
+
+nWKid(person);
+nwHawks(person);
+
+var greet = 'Hello';
+var greet = 'Hola';
+
+var english = {};
+var spanish = {};
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola!';
+console.log(english);
+console.log(spanish);
+
+//Namespace container with multiple objects
+
+//few deeper levels
+
+var english_greetings = {
+	greetings : {
+		basic : 'hello',
+		casual : 'sup',
+		formal : 'Nice to meet you'
+	}
+};
+var spanish_greetings = {
+	greetings : {
+		basic : 'hola',
+		casual : 'Que pasa',
+		formal : 'Mucho gusto'
+	}
+};
+
+function language_greetings(object1, object2){
+	console.log("It\'s much more fun to say " + object2.greetings.casual + ', then, ' + object1.greetings.casual);
+}
+language_greetings(english_greetings, spanish_greetings);
+
+//container object to keep greetings objects separate within language greetings
+
